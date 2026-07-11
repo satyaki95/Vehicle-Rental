64fcc40488d5627aa6e9c87239350c03d2ed7464
satyaki95 <www.satyaki95@gmail.com>
2026-07-12 01:17:03 +0530

    set context

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,22 +1,47 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
+
+axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
 export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const currency = import.meta.env.VITE_CURRENCY;
 
-    const value = {
+  const [token, setToken] = useState(null);
+  const [user, setUser] = useState(null);
+  const [isOwner, setIsOwner] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+  const [pickupDate, setPickupDate] = useState("");
+  const [returnDate, setReturnDate] = useState("");
 
+  const [cars, serCars] = useState([]);
+
+  // Function to check if user is logged in
+  const fetchUser = async () => {
+    try {
+      const { data } = await axios.get("/api/user/data");
+
+      if (data.success) {
+        setUser(data.user);
+        setIsOwner(data.user.role === "owner");
+      } else {
+        navigate("/");
+      }
+    } catch (error) {
+      toast.error(error.message);
     }
-  return (
-  <AppContext.Provider value={value}>
-    {children}
-    </AppContext.Provider>
-  );
+  };
+
+  const value = { navigate, currency };
+
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
-
 export const useAppContext = () => {
-
-
-    return useContext(AppContext)
-}
+  return useContext(AppContext);
+};
