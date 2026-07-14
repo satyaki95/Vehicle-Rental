461703aed59b9501f0685e3158e604c55319c248
satyaki95 <www.satyaki95@gmail.com>
2026-07-15 01:36:01 +0530

    animation

--- client/src/components/FeaturedSection.jsx
+++ client/src/components/FeaturedSection.jsx
@@ -3,28 +3,52 @@ import { useAppContext } from "../context/AppContext";
 import CarCard from "./CarCard";
 import Title from "./Title";
 import { useNavigate } from "react-router-dom";
+import { motion } from "motion/react";
 
 const FeaturedSection = () => {
   const navigate = useNavigate();
   const { cars } = useAppContext();
+
   return (
-    <div className="flex flex-col items-center py-24 px-6 md:px-16 lg:px-24 xl:px-32">
-      <div>
+    <motion.div
+      initial={{ y: 40, opacity: 0 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1, ease: "easeOut" }}
+      className="flex flex-col items-center py-24 px-6 md:px-16 lg:px-24 xl:px-32"
+    >
+      <motion.div
+        initial={{ y: 20, opacity: 0 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.5 }}
+      >
         <Title
           title="Featured Vehicles"
           subTitle="Explore our selection of premium vehicles for your next adventure."
         />
-      </div>
+      </motion.div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-18">
+      <motion.div
+        initial={{ y: 100, opacity: 0 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, delay: 0.5 }}
+        className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-18"
+      >
         {cars.slice(0, 6).map((car) => (
-          <div key={car._id}>
+          <motion.div
+            initial={{ scale: 0.95, opacity: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.4, ease: "easeOut" }}
+            key={car._id}
+          >
             <CarCard car={car} />
-          </div>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
 
-      <button
+      <motion.button
+        initial={{ y: 20, opacity: 0 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, delay: 0.6 }}
         onClick={() => {
           navigate("/cars");
           scrollTo(0, 0);
@@ -32,8 +56,8 @@ const FeaturedSection = () => {
         className="flex items-center justify-center gap-2 px-6 py-2 border border-borderColor hover:bg-gray-50 rounded-md mt-18 cursor-pointer"
       >
         Explore all cars <img src={assets.arrow_icon} alt="arrow" />
-      </button>
-    </div>
+      </motion.button>
+    </motion.div>
   );
 };
 
--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -89,17 +89,28 @@ const Hero = () => {
             />
           </div>
         </div>
-        <button className="flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer">
+        <motion.button
+          whileHover={{ scale: 1.05 }}
+          whileTap={{ scale: 0.95 }}
+          className="flex items-center justify-center gap-1 px-9 py-3 max-sm:mt-4 bg-primary hover:bg-primary-dull text-white rounded-full cursor-pointer"
+        >
           <img
             src={assets.search_icon}
             alt="search"
             className="brightness-300"
           />
           Search
-        </button>
+        </motion.button>
       </motion.form>
 
-      <img src={assets.main_car} alt="car" className="max-h-74" />
+      <motion.img
+        initial={{ y: 100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+        transition={{ duration: 0.8, delay: 0.6 }}
+        src={assets.main_car}
+        alt="car"
+        className="max-h-74"
+      />
     </motion.div>
   );
 };
