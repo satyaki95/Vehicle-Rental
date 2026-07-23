d79ef4e1a8504eb71fc3facb180afacded29cc00
satyaki95 <www.satyaki95@gmail.com>
2026-07-23 19:38:47 +0530

    delete car

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -240,11 +240,9 @@ export const deleteVehicle = async (req, res) => {
       return res.json({ success: false, message: "Vehicle not found" });
     }
 
-    vehicle.owner = null;
-    vehicle.isAvailable = false;
-    await vehicle.save();
+    await Vehicle.findByIdAndDelete(id);
 
-    res.json({ success: true, message: "Vehicle Removed" });
+    res.json({ success: true, message: "Vehicle deleted" });
   } catch (error) {
     console.log(error.message);
     res.json({ success: false, message: error.message });
